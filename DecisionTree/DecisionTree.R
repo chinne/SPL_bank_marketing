8bb95ad79657398cce58dbbfb72ceaaa64585502
chinne <christoph.linne@student.hu-berlin.de>
2017-03-14 15:50:31 +0100

    formatR

--- DecisionTree/DecisionTree.R
+++ DecisionTree/DecisionTree.R
@@ -16,23 +16,23 @@ library(mlr)
 
 # read data
 bank = read.csv("bankclean.csv")
-bank = bank[,-1]
+bank = bank[, -1]
 
 
 # split the data for training and testing
 
 set.seed(124)
-n <- nrow(bank) 
-sample.size <- ceiling(n*0.8) 
-idx.train <- sample(n, sample.size) 
+n <- nrow(bank)
+sample.size <- ceiling(n * 0.8)
+idx.train <- sample(n, sample.size)
 
-data_train = bank[idx.train,]
-data_test = bank[-idx.train,]
+data_train = bank[idx.train, ]
+data_test = bank[-idx.train, ]
 
-# load the balanced training set and 
+# load the balanced training set and
 
 balancedTrain = read.csv("balancedTrain.csv")
-balancedTrain = balancedTrain[,-1]
+balancedTrain = balancedTrain[, -1]
 
 levels(balancedTrain$default) = levels(bank$default)
 
@@ -40,38 +40,36 @@ levels(balancedTrain$default) = levels(bank$default)
 # build the decison tree model
 
 # pre-pruning the tree in case of overfitting
-rpart.control= rpart.control(minsplit = 5, minbucket = round(5/3),
-                             maxdepth = 4, 
-                             cp = 0.001)
+rpart.control = rpart.control(minsplit = 5, minbucket = round(5/3), maxdepth = 4, 
+    cp = 0.001)
 # decision tree model
-dtm<-rpart(y~.,data = balancedTrain,
-               method = "class",
-               parms = list(split="information"), 
-               control = rpart.control)
+dtm <- rpart(y ~ ., data = balancedTrain, method = "class", parms = list(split = "information"), 
+    control = rpart.control)
 
 # plot the decision tree model
-rpart.plot(dtm,type = 1,cex = 0.6, extra = 104, tweak=.8, faclen = 3)
+rpart.plot(dtm, type = 1, cex = 0.6, extra = 104, tweak = 0.8, faclen = 3)
 
 # test the classifier in testing dataset
 
-pred_dtm = predict(dtm, newdata = data_test,type = "prob")
+pred_dtm = predict(dtm, newdata = data_test, type = "prob")
 
 # set the threshold to see the Confusion Matrix
 threshold = 0.5
-pred_class = as.matrix(factor(ifelse(pred_dtm[,2] > threshold,"yes","no")))
-confusionMatrix(pred_class, data_test$y,positive = "yes")
+pred_class = as.matrix(factor(ifelse(pred_dtm[, 2] > threshold, "yes", 
+    "no")))
+confusionMatrix(pred_class, data_test$y, positive = "yes")
 
 
 
 # plot the ROC and calculate AUC
 
 
-roc_dtm = roc(pred_dtm[,2], data_test$y)
+roc_dtm = roc(pred_dtm[, 2], data_test$y)
 roc_dtm
 
-plot(roc_dtm, main = "ROC Curve for Decision Tree Model", col="red")
+plot(roc_dtm, main = "ROC Curve for Decision Tree Model", col = "red")
 
-auc_dt_b = auc(roc_dtm,min=0,max=1)
+auc_dt_b = auc(roc_dtm, min = 0, max = 1)
 auc_dt_b
 
 
